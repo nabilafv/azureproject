bba73816defe8ec65251becd15c702e6876f0a68
Nabila Febri Viola <nabilafebriviola@gmail.com>
2019-07-17 19:07:53 +0700

    add whitenoise

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'azureproject/static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
